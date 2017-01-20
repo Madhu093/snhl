fb7f4b8369142448b4c5c8546a475015416ee18f
Kurapati, Madhu <madhu.devlp09@gmail.com>
2017-01-19 20:47:15 -0800

    Add on click of button in selected_Story

--- app/src/main/java/com/example/kurapma/snhl/adapter/StoryAdapter.java
+++ app/src/main/java/com/example/kurapma/snhl/adapter/StoryAdapter.java
@@ -7,9 +7,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.kurapma.snhl.R;
+import com.example.kurapma.snhl.activity.DonateActivity;
 import com.example.kurapma.snhl.activity.SelectedStoryActivity;
 import com.example.kurapma.snhl.model.StoryData;
 import com.squareup.picasso.Picasso;
@@ -61,7 +63,7 @@ public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.ViewHolder>{
     class ViewHolder extends RecyclerView.ViewHolder{
         public CardView mCardView;
         public ImageView mImageView;
-
+        public Button donate_button;
         public ViewHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +78,14 @@ public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.ViewHolder>{
 
             mCardView = (CardView) itemView.findViewById(R.id.story_card_view);
             mImageView = (ImageView) itemView.findViewById(R.id.story_iv_image);
+            donate_button = (Button) itemView.findViewById(R.id.donote_button);
+
+            donate_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    view.getContext().startActivity(new Intent(view.getContext(), DonateActivity.class));
+                }
+            });
         }
     }
 }
