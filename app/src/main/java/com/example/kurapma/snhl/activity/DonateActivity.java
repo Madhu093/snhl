6efb72efb904421d410e9b3f78f053ee16ae4509
Kurapati, Madhu <madhu.devlp09@gmail.com>
2017-01-19 20:40:24 -0800

    edit donate layout

--- app/src/main/java/com/example/kurapma/snhl/activity/DonateActivity.java
+++ app/src/main/java/com/example/kurapma/snhl/activity/DonateActivity.java
@@ -1,9 +1,7 @@
 package com.example.kurapma.snhl.activity;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,7 +14,6 @@ import android.widget.EditText;
 
 import com.example.kurapma.snhl.R;
 import com.example.kurapma.snhl.model.PayPalConfig;
-import com.facebook.login.LoginManager;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -56,7 +53,7 @@ public class DonateActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment);
+        setContentView(R.layout.activity_donate);
 
         Intent intent = new Intent(this, PayPalService.class);
 
